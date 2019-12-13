56067ac8a811d83cf00f696c914b1f764340bdcf
Claire Ramos <Bella@Claires-MBP.fios-router.home>
2019-12-13 18:38:55 -0500

    Added classes for items

--- sweets.rb
+++ sweets.rb
@@ -1,22 +1,45 @@
+
 class Cookie
-    attr_reader :name
-    def initialize(name)
-        @cookie_name = name
+    attr_reader :name, :price
+    def initialize(name, price)
+        @name = name
+        @price = price
     end
 end
 
 class Cupcake
-    attr_reader :name
-    def initialize(name)
-        @cupcake_name = name
+    attr_reader :name, :price
+    def initialize(name, price)
+        @name = name
+        @price = price
     end
 end
 
 class Cake
-    attr_reader :name
-    def initialize(name)
-        @cake_name = name
+    attr_reader :name, :price
+    def initialize(name, price)
+        @name = name
+        @price = price
     end
 end
 
-chococup = Cupcake.new("Chocolate Cupcake")
+# Cookie classes
+
+Chocochip = Cookie.new("Chocolate-Chip Cookie", "1.00")
+Sugarcookie = Cookie.new("Sugar Cookies", "0.50")
+Macaroons = Cookie.new("Macaroons", "1.50")
+Strawberrycookie = Cookie.new("Vanilla-Strawberry Smothers", "2.00")
+
+# Cupcake classes
+
+Chococup = Cupcake.new("Chocolate Cupcake", "1.50")
+Strawcup = Cupcake.new("Strawberry Sunrise", "2.00")
+Bluecup = Cupcake.new("Blueberry Dreams", "2.00")
+Maccup = Cupcake.new("Macadami-cup", "2.00")
+
+# Cake classes
+
+Chocostrawcake = Cake.new("Choco-straw Cake", "4.25")
+Chocomoussecake = Cake.new("Chocolate w/ Mousse Cake", "5.50")
+Cheesecake = Cake.new("Cheesecake", "3.00")
+Smores = Cake.new("Smores", "4.00")
